84d9d181f185592f87b43daf33abf78a7f2e1349
Urban Tratnik <urban.tratnik22@gmail.com>
2018-07-02 17:20:17 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const navButton = document.querySelector(".menu_btn");
+const phoneMenu = document.querySelector(".small_menu");
+const wrapperMenu = document.querySelector(".menu");
+
+navButton.addEventListener("click", function () {
+    wrapperMenu.classList.toggle("small_menu");
+});
